e8b68c93f0bc448ec49a5a9fb108ec89ffff5cb2
CDiala <dialachibuzo@yahoo.com>
2021-10-28 02:05:41 +0100

    Desktop card component fully designed.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let deviceWidth = window.innerWidth;
-console.log(deviceWidth);
+// console.log(deviceWidth);
 
 let navOne = document.querySelector("#nav-1");
 let navTwo = document.querySelector("#nav-2");
 let navThree = document.querySelector("#nav-3");
 
-window.onload = getNewDimension();
+window.onload = getNewDimension;
 window.onresize = getNewDimension;
 
 function getNewDimension() {
@@ -18,5 +18,44 @@ function getNewDimension() {
     navOne.textContent = "Navigation Link One";
     navTwo.textContent = "Navigation Link Two";
     navThree.textContent = "Navigation Link Three";
+  }
+}
+
+function getMe(ev) {
+  let isLiked = false;
+  let likeReview = document.querySelector(".card_review");
+  let likeCount = Number.parseFloat(
+    likeReview.textContent.split(" ")[0].replaceAll(",", "")
+  );
+  let clsId = ev.target.id[ev.target.id.length - 1] - 1;
+  let element = document.getElementById(ev.target.id);
+
+  for (let i = 0; i < 5; i++) {
+    let prevStar = document.getElementById("star-" + (i + 1));
+    if (prevStar.classList.contains("star_fill")) {
+      isLiked = true;
+    }
+  }
+
+  if (element.classList.contains("star_fill")) {
+  } else {
+    for (let i = 0; i <= clsId; i++) {
+      let prevStar = document.getElementById("star-" + (i + 1));
+      prevStar.src = "images/star_fill.svg";
+      prevStar.classList.add("star_fill");
+      console.log("fill", prevStar);
+    }
+  }
+
+  for (let i = clsId + 1; i <= 4; i++) {
+    let prevStar = document.getElementById("star-" + (i + 1));
+    prevStar.src = "images/star_empty.svg";
+    prevStar.classList.remove("star_fill");
+    console.log("empty", prevStar);
+  }
+
+  if (!isLiked) {
+    likeCount++;
+    likeReview.textContent = likeCount.toLocaleString("en-US") + " Reviews";
   }
 }
